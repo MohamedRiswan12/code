c5ad17f5c392d3d68f16a72989957a9a04d5cf6a
Mohamed Riswan <Mohamed Riswan@LAPTOP-DCURII00>
2022-02-08 19:10:02 +0530

    arithmetic operations

---
+++ eve/src/eve/arithmeticOperations.java
@@ -0,0 +1,16 @@
+package eve;
+
+public class arithmeticOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=200;
+int b=10;
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
+System.out.println(a%b);
+	}
+
+}
