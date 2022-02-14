2a5e316be7a74ce14a98f6c47749ceae40831b4f
Mohamed Riswan <Mohamed Riswan@LAPTOP-DCURII00>
2022-02-14 13:08:27 +0530

    conditional operators and ascii number

---
+++ eve/src/eve/asciiNumber.java
@@ -0,0 +1,15 @@
+package eve;
+
+public class asciiNumber {
+
+	public static void main(String[] args) {
+		
+	     char ch = 'k';
+		 int ascii = ch; 
+		 // You can also cast char to int
+		//int castAscii = (int) ch; 
+		 System.out.println("The ASCII value of " + ch + " is: " + ascii); 
+		// System.out.println("The ASCII value of " + ch + " is: " + castAscii); 
+	}
+
+}
---
+++ eve/src/eve/conditional_operator.java
@@ -0,0 +1,37 @@
+package eve;
+
+public class conditional_operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+int num=10%2;
+boolean boo=(num==0);
+String result=boo?"odd numbers":"even numbers";
+System.out.println(result);
+int a=20;
+int b=30;
+if(a>b) {
+	System.out.println("a is greater than b");
+	
+}else {
+	System.out.println("b is greater than a");
+}
+String name= "irfan";
+switch(name) {
+case "irfan":
+	System.out.println("My Name Is Irfan");
+	break;
+case "goutham":
+	System.out.println("My Name Is Goutham");
+	break;
+case "riswan":
+	System.out.println("My Name Is Riswan");
+default:
+	System.out.println("name not found");
+	
+}
+
+	}
+
+}
