f3c312c08cc2cfb486afe86b7ffb79a81e72b16e
Mohamed Riswan <Mohamed Riswan@LAPTOP-DCURII00>
2022-02-27 19:02:08 +0530

    pyramid examples

---
+++ eve/src/eve/pyramidExample1.java
@@ -0,0 +1,16 @@
+package eve;
+
+public class pyramidExample1 {
+
+	public static void main(String[] args) {
+		int term=6;
+		for(int i=1;i<=term;i++) {
+			for(int j=term;j>=1;j--) {
+				System.out.println("* ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
